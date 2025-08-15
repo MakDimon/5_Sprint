6a553b2dcd74c26e7b901a38f63de1b4e8ad6d51
Dmitrii Makhonin <dima@mhnn.ru>
2025-08-15 09:49:59 +0300

    1_iteration

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -1,9 +1,17 @@
 package personaldata
 
+import "fmt"
+
+// Personal структура содержит имя, вес и рост
 type Personal struct {
 	// TODO: добавить поля
+	Name   string
+	Weight float64
+	Height float64
 }
 
+// метод имени, веса и роста на экран
 func (p Personal) Print() {
 	// TODO: реализовать функцию
+	fmt.Printf("Имя: %s\nВес: %.2f кг.\nРост: %.2f м.\n", p.Name, p.Weight, p.Height)
 }
